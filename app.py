ba8621a954290a30840777dcc661450bf060ae43
James Pace <jamesearlpace@gmail.com>
2025-10-15 17:12:03 -0700

    update text

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.title("AI Agent Demooo")
-st.write("Hello world! If you see this, Streamlit works. SO COOL")
+st.title("AI Agent Demo")
+st.write("Hello world! If you see this, Streamlit works...")
 
